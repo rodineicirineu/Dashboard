9256395a75ae2098b557dddc408f21422520332a
Rodinei Roger Cirineu <81439723+rodineicirineu@users.noreply.github.com>
2021-04-15 14:35:07 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Dashboard
+![Dashboard](https://user-images.githubusercontent.com/81439723/114913464-99568800-9df7-11eb-8b0f-b740043d363a.gif)
+
+> Status: Concluded ✔️
+
+
+## Technologies and IDE Used:
+
+<table>
+  <tr>
+    <td> Visual Studio Code </td>
+    <td> HTML </td>
+    <td> CSS </td>
+    <td> Javascript </td>
+  </tr>
+    <tr>
+    <td> 1.54 </td>
+    <td> 5 </td>
+    <td> 3 </td>
+    <td> 1.7 </td>
+  </tr>
+</table>
+
+
+## How to run the application:
+
+1) Download the files
+2) Unzip the files
+3) Run the index.html file
+
+
---
+++ js/script.js
@@ -0,0 +1,125 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn-dashboard')) {
+      var dropdowns = document.getElementsByClassName("btndropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  /* Range Slider */
+  var inputLeft = document.getElementById("input-left");
+  var inputRight = document.getElementById("input-right");
+  
+  var thumbLeft = document.querySelector(".slider > .thumb.left");
+  var thumbRight = document.querySelector(".slider > .thumb.right");
+  var range = document.querySelector(".slider > .range");
+  
+  function setLeftValue() {
+    var _this = inputLeft,
+      min = parseInt(_this.min),
+      max = parseInt(_this.max);
+  
+    _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  
+    var percent = ((_this.value - min) / (max - min)) * 100;
+  
+    thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
+  }
+  setLeftValue();
+  
+  function setRightValue() {
+    var _this = inputRight,
+      min = parseInt(_this.min),
+      max = parseInt(_this.max);
+  
+    _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  
+    var percent = ((_this.value - min) / (max - min)) * 100;
+  
+    thumbRight.style.right = (100 - percent) + "%";
+    range.style.right = (100 - percent) + "%";
+  }
+  setRightValue();
+  
+  
+  inputLeft.addEventListener("input", setLeftValue);
+  inputRight.addEventListener("input", setRightValue);
+  
+  inputLeft.addEventListener("mouseover", function() {
+    thumbLeft.classList.add("hover");
+  });
+  inputLeft.addEventListener("mouseout", function() {
+    thumbLeft.classList.remove("hover");
+  });
+  inputLeft.addEventListener("mousedown", function() {
+    thumbLeft.classList.add("active");
+  });
+  inputLeft.addEventListener("mouseup", function() {
+    thumbLeft.classList.remove("active");
+  });
+  
+  inputRight.addEventListener("mouseover", function() {
+    thumbRight.classList.add("hover");
+  });
+  inputRight.addEventListener("mouseout", function() {
+    thumbRight.classList.remove("hover");
+  });
+  inputRight.addEventListener("mousedown", function() {
+    thumbRight.classList.add("active");
+  });
+  inputRight.addEventListener("mouseup", function() {
+    thumbRight.classList.remove("active");
+  });
+
+
+
+  var slider1 = document.getElementById("input-left");
+  var output1 = document.getElementById("left");
+  var slider = document.getElementById("input-right");
+  var output = document.getElementById("right");
+  output.innerHTML = slider.value;
+  output1.innerHTML = slider1.value;
+
+  slider1.oninput = function() {
+    output1.innerHTML = this.value;
+  }
+  
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+
+  /* Pagination */
+  var items = $($('.group-card .card '));
+  var numItems = 20;
+  var perPage = 4;
+
+  items.slice(perPage).hide();
+
+  $('#pagination-container').pagination({
+    items : numItems,
+    itemsOnPage : perPage,
+    prevText: "<",
+    nextText: ">",
+    onPageClick : function (pageNumber) {
+      var showFrom = perPage * (pageNumber - 1);
+      var showTo = showFrom + perPage;
+      items.hide().slice(showFrom, showTo).show(500);
+    }
+  })
+
+  /* Calendar */
+  var example = flatpickr('#flatpickr');
